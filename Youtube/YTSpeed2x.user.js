02d547beccfa30ebae0a810d0b2628b59e21153e
FabianPastor <FabianPastor@users.noreply.github.com>
2022-04-22 21:36:11 +0200

    Refactored some parts of YTSpeed2x script.

--- Youtube/YTSpeed2x.user.js
+++ Youtube/YTSpeed2x.user.js
@@ -12,8 +12,8 @@
 // ==/UserScript==
 
 (function () {
-  var timeout = 1 * 1000; // 2 seconds for each check
-  
+  var timeout = 2 * 1000; // 2 seconds for each check
+
   var sliderIntervals = 1000;
   var sliderMax = 6;
   var sliderMin = 0;
@@ -21,61 +21,95 @@
   var sliderMult = (((sliderMax) - (sliderMin)) / sliderIntervals);
   var sliderDefMult = sliderDef / sliderMult;
   var video = null;
+  var center = null;
+  var slider = null;
+  var divslider = null;
 
+  function updateSliderValue(value,force = false){
+    value = (value > sliderMax)?sliderMax:value;
+    value = (value < sliderMin)?sliderMin:value;
+
+    if(!force && value === getCurrentSliderValue()){
+      return;
+    }
+
+    slider.value = value/sliderMult;
+    slider.nextSibling.innerHTML = value.toFixed(5);
+    video.playbackRate = value;
+  }
+  function getCurrentSliderValue(){
+    return sliderMult * slider.value;
+  }
 
   function sliderLoader() {
-    video.playbackRate = sliderDef;
-    var center = document.getElementById("center");
-    var divslider = document.createElement("div");
+
+    center = document.getElementById("center");
+    divslider = document.createElement("div");
+
     divslider.id = "div_slider";
     divslider.innerHTML = '<input id="slider_speed" type="range" min="0" max="' + sliderIntervals + '" value="' + sliderDefMult + '">' +
-      '<span style="background-color:#fff;font-size: 20px;border-radius: 20px;padding: 2px 8px;">' + sliderDef.toFixed(5) + '</span>'
+    '<span style="background-color:#fff;font-size: 20px;border-radius: 20px;padding: 2px 8px;">' + sliderDef.toFixed(5) + '</span>'
     center.parentElement.insertBefore(divslider, center.nextSibling);
-
-    var slider = document.getElementById("slider_speed");
+    slider = document.getElementById("slider_speed");
+    updateSliderValue(sliderDef);
 
 
     slider.oninput = function (element) {
-      var value = sliderMult * slider.value;
-      unsafeWindow.console.log("New SpeedValue: " + value);
-      slider.nextSibling.innerHTML = value.toFixed(5);
-      video.playbackRate = value;
+      var value = getCurrentSliderValue();
+      unsafeWindow.console.log("New SpeedValue onInput: " + value);
+      updateSliderValue(value, true);
     }
 
-    slider.ondblclick = function (element) {
-      var value = 1;
-      unsafeWindow.console.log("New SpeedValue: " + value);
-      slider.value = value / sliderMult;
-      slider.nextSibling.innerHTML = value.toFixed(5);
-      video.playbackRate = value;
+    divslider.ondblclick = function (element) {
+      var value = Math.round(getCurrentSliderValue());
+      if(value == 1){
+        value = 2;
+      }else{
+        value = 1;
+      }
+
+      unsafeWindow.console.log("New SpeedValue onDBLClick: " + value);
+      updateSliderValue(value);
     }
+
+    divslider.onwheel = function(ev){
+      unsafeWindow.console.log("Detected onWheel change: "+ ev);
+
+      if(ev.deltaY === 0){
+          return;
+      }
+      var value = getCurrentSliderValue();
+      if((ev.deltaY) < 0){
+          value = value + (1/16);
+      }else{
+          value = value - (1/16);
+      }
+      ev.preventDefault();
+
+      updateSliderValue(value);
+    }
+
     unsafeWindow.onkeypress = function (ev) {
       unsafeWindow.console.log("Key pressed: " + ev.key);
-      var value = sliderMult * slider.value;
-      var newValue = value;
+      var value = getCurrentSliderValue();
       switch (ev.key) {
         case "+":
-          newValue = value + (1 / 4);
+          value = value + (1 / 4);
           break;
         case "-":
-          newValue = value - (1 / 4);
+          value = value - (1 / 4);
           break;
         case "/":
-          newValue = 1;
+          value = 1;
           break;
         case "*":
-          newValue = 2;
+          value = 2;
           break;
         default:
           return;
       }
       ev.preventDefault();
-      newValue = (newValue > sliderMax) ? sliderMax : newValue;
-      newValue = (newValue < sliderMin) ? sliderMin : newValue;
-      slider.value = newValue / sliderMult;
-      slider.nextSibling.innerHTML = newValue.toFixed(5);
-      video.playbackRate = newValue;
-
+      updateSliderValue(value);
     }
 
   }
@@ -83,19 +117,21 @@
   function checkVideo() {
     try {
       video = document.getElementsByTagName("video")[0];
+
       clearInterval(unsafeWindow.setSpeed2x);
       unsafeWindow.setSpeed2x = true;
       sliderLoader();
     } catch (e) {
       unsafeWindow.console.log(e);
     }
-    
+/*
     if(unsafeWindow.setSpeed2xCount > 30){
       clearInterval(unsafeWindow.setSpeed2x);
       unsafeWindow.setSpeed2x = true;
       return;
     }
     unsafeWindow.setSpeed2xCount++
+*/
   }
 
   if (typeof unsafeWindow.setSpeed2x === 'undefined') {
