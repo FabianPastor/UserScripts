b9528480494dc1e7c4ae6c5820693aae05a8fb45
FabianPastor <FabianPastor@users.noreply.github.com>
2024-02-22 09:51:11 +0100

    Actualizado Trello/StyleMod.user.js
    Arreglos de bugs y mejoras.

--- Trello/StyleMod.user.js
+++ Trello/StyleMod.user.js
@@ -3,7 +3,7 @@
 // @description  Small Trello CSS mod to change a few minor Style Properties.
 // @author       FabianPastor
 // @namespace    http://fabi.servehttp.com/
-// @version      0.01
+// @version      0.10
 // @updateURL    https://raw.githubusercontent.com/FabianPastor/UserScripts/master/Trello/StyleMod.user.js
 // @downloadURL  https://raw.githubusercontent.com/FabianPastor/UserScripts/master/Trello/StyleMod.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=trello.com
@@ -14,18 +14,29 @@
     'use strict';
 
     function applyStyles(){
-        let cardSize    = 1000;
-        let sideColSize = 200;
-        let mainColSize = cardSize - sideColSize - 50;
-        
-        let card    = document.querySelector("div.window.editor-sticky-toolbar");
-        let mainCol = document.querySelector("div.window-main-col");
-        let sideCol = document.querySelector("div.window-sidebar");
-        
-        
-        card.style.width    = cardSize+"px";
-        mainCol.style.width = mainColSize+"px";
-        sideCol.style.width = sideColSize+"px";
+        const cardSize    = Math.round(window.innerWidth*0.8);
+        const sideColSize = 200;
+        const mainColSize = cardSize - sideColSize - 50;
+        let card = null;
+      
+        card    = document.querySelector("#layer-manager-card-back>div>div>div");
+        if(card == null){
+          card    = document.querySelector("div.window.editor-sticky-toolbar");
+        }
+
+        if(card !== null){
+            const mainCol = document.querySelector("div.window-main-col");
+            const sideCol = document.querySelector("div.window-sidebar");
+
+            try{
+                card.style.width    = cardSize+"px";
+                mainCol.style.width = mainColSize+"px";
+                sideCol.style.width = sideColSize+"px";
+            }catch(e){
+                //alert('Hubo un error al tratar de agrandar la tarjeta.');
+            }
+        }
+
     }
 
     let applyStylesInterval = setInterval(applyStyles,1000);
