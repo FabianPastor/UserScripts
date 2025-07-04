2b08cfbbb48c81a70e28de52a159efafe0b15376
FabianPastor <FabianPastor@users.noreply.github.com>
2025-07-04 11:34:21 +0200

    Update StyleMod.user.js

--- Trello/StyleMod.user.js
+++ Trello/StyleMod.user.js
@@ -3,7 +3,7 @@
 // @description  Small Trello CSS mod to change a few minor Style Properties.
 // @author       FabianPastor
 // @namespace    http://fabi.servehttp.com/
-// @version      0.12
+// @version      0.13
 // @grant        GM_addStyle
 // @updateURL    https://raw.githubusercontent.com/FabianPastor/UserScripts/master/Trello/StyleMod.user.js
 // @downloadURL  https://raw.githubusercontent.com/FabianPastor/UserScripts/master/Trello/StyleMod.user.js
@@ -15,25 +15,31 @@
 
 function applyStyle(){
     GM_addStyle(`
-          ._UvaHK6fukmTc4.sfCjcMKVjES0Hl.t_6PqKcL7DA8Qa{
+          ._UvaHK6fukmTc4.sfCjcMKVjES0Hl.t_6PqKcL7DA8Qa,
+          .BvYMUSHQ3VZhSV.n2JiYx2TrpRj9Z.udR7hhdNKnONkc{
             width: 100vw !important;
           }
 
-          .u4mjknssOvTvF8 .OftUJMY9A1bsMS{
+          .u4mjknssOvTvF8.OftUJMY9A1bsMS,
+          .JXDJgHrIm3NIAi .bgl1URnPxrvOPl
+          {
             width:5% !important;
           }
 
-          .u4mjknssOvTvF8 .kli7aIGeyXSbYG{
+          .u4mjknssOvTvF8.kli7aIGeyXSbYG,
+          .JXDJgHrIm3NIAi .FyDYWBAU_FzZrU{
             width: 90% !important;
             min-width: inherit !important;
             max-width: inherit !important;
           }
 
-          .q5xxNU7ASO2fsR, .q5xxNU7ASO2fsR .N4ktAjtOpkJvy0{
+          .q5xxNU7ASO2fsR, .q5xxNU7ASO2fsR.N4ktAjtOpkJvy0,
+          .w7hbB5D5vQdlht, .w7hbB5D5vQdlht .E59SuLLYHMwjAT{
             width: 95% !important;
           }
 
-          .q5xxNU7ASO2fsR{
+          .q5xxNU7ASO2fsR,
+          .w7hbB5D5vQdlht{
             flex: 1 1 !important;
           }
         `);
